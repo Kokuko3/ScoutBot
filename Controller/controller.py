7cc5cb8fb0633e467780832040b6e15cb94b7dda
Kokuko3 <mcdaniel.121@wright.edu>
2024-08-07 00:04:10 -0400

    Updates

--- Controller/controller.py
+++ Controller/controller.py
@@ -12,13 +12,20 @@ def set_speed_left(pos):
     speed = pos.y
     kit.motor4.throttle = speed
 
-kit = MotorKit(i2c=board.I2C())
-bd = Bluedot(rows=1,col=2)
+def stop_right():
+    kit.motor1.throttle = 0
 
+def stop_left():
+    kit.motor4.throttle = 0
+
+kit = MotorKit(i2c=board.I2C())
+
+bd = Bluedot(rows=1,col=2)
+bd.square = True
+bd.color = "#808080"
 bd[0,0].when_moved = set_speed_left
 bd[1,0].when_moved = set_speed_right
+bd[0,0].when_released = stop_left
+bd[1,0].when_released = stop_right
 
 pause()
-
-kit.motor1.throttle = 0
-kit.motor4.throttle = 0
